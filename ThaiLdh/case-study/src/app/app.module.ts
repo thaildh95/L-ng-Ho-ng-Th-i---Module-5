4807525184dfa107002ebdb4cc71d966d666054f
thaildh95 <thaildh.rtbed@gmail.com>
2022-09-25 14:44:44 +0700

    dasds

--- ThaiLdh/case-study/src/app/app.module.ts
+++ ThaiLdh/case-study/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './home/nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './customer/list/list.component';
+import { NgSComponent } from './home/ng-s/ng-s.component';
+import { AddComponent } from './customer/add/add.component';
+import { EditComponent } from './customer/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    ListComponent,
+    NgSComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
