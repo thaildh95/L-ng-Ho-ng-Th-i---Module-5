96f64093f962dc6006dc7d6445b07480bb9803f7
thaildh95 <thaildh.rtbed@gmail.com>
2022-10-12 11:16:55 +0700

    u

---
+++ testttt/src/main/java/com/example/testttt/TesttttApplication.java
@@ -0,0 +1,13 @@
+package com.example.testttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesttttApplication.class, args);
+    }
+
+}
---
+++ testttt/src/test/java/com/example/testttt/TesttttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
